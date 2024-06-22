f8887c8cc535caf0c9cb7dfcd8de94aadc94eb3f
somberlyx <lybasiddiqui02@gmail.com>
2024-06-22 21:50:00 +0400

    create an api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 const app = express();
@@ -12,7 +12,9 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log(err);
 })
 
-app.listen(5173, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
   }
 );
+
+app.use('/api/user', userRouter) 
